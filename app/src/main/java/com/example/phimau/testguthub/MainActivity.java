9f3956ca82219de68f3fd7994ab4016179092f69
phimhph04071 <santafe9xyb@gmail.com>
2016-08-02 16:22:53 +0800

    ssssssssss

--- app/src/main/java/com/example/phimau/testguthub/MainActivity.java
+++ app/src/main/java/com/example/phimau/testguthub/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.phimau.testguthub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // day la phi mau vo dich thien ha
     }
 }
